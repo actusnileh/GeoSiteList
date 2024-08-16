75effcd2d50229ac6deddce5583ecba6b97b8877
actusnileh <actusnileh@gmail.com>
2024-08-16 12:29:18 +0300

    modified:   src/routing/Router.tsx

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -1,7 +1,7 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { HomePage, AboutPage } from "../pages";
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
     {
         path: "/",
         element: <HomePage />,
