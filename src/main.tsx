8680421dc732c156e71d60884fb19f3dbabf7ce2
actusnileh <actusnileh@gmail.com>
2024-08-16 15:57:34 +0300

    Fix routers

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './routing/App';
 import "./shared/global_styles/unstyle.css";
+import "./shared/404.html";
 import '@mantine/core/styles.css';
 import './shared/global_styles/fonts.css';
 
